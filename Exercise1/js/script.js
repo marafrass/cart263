856d123a2b6a5789cf85ff0b55a3967225fd1f91
Martin <martinhanses@hotmail.com>
2020-01-18 12:52:03 -0500

    Assignment 1: Added all the optional challenges
    - Text is now displayed in pixels
    - Pixels can now be clicked and removed
    - Added important and relevant intro comment

--- Exercise1/js/script.js
+++ Exercise1/js/script.js
@@ -4,10 +4,28 @@
 
 Pixel painter
 Pippin Barr
+with edits and additions by
+Martin Hanses
 
-additions by Martin Hanses
+Pixel painting remains a touchy subject for pixel purists. Some believe that
+pixels should remain their natural color; the deep crimson blue they are
+generally associated with at the time of harvest. As regulations hit pixel
+plantations and farms harder and harder, many landowners and farmers have chosen to
+genetically modify their pixels, giving off new, interesting and eye-catching
+colors that surprise and intrigue the market. Among these are variations of
+the typical reds and blues, but also the highly unusual and, some say, unnatural
+green.
 
-A small DOM-based program for "painting" on div-based pixels.
+  With the pixel market booming due to trending hashtags and Steam Direct,
+consumers may find themselves asking for a natural alternative - something
+pixel and RGB-producers consider troublesome. "It's just not financially
+viable," says Barracuda "Scorched Earth" Pinsley, a local third-generation pixel
+farmer who just happened to stumble into the newsroom while we we're investigating
+the story. He was promptly escorted out after being violently tased, as is
+tradition, but his charming yokel slurring coupled with this somehow coherent sentence
+was still relevant enough to make it into this article. You can argue that this
+kind of circumstantial reporting is poor journalism, but we're not to blame -
+this has been the state of news media since the early 90s.
 
 *********************************************************************/
 
@@ -17,6 +35,7 @@ const PIXEL_REVERT_DELAY = 1000;
 const DEFAULT_COLOR = 'black';
 
 let rotation = 0;
+let currentKey = "";
 
 // Set up our starting function for when the page loads
 window.onload = setup;
@@ -32,14 +51,21 @@ function setup() {
     let pixel = document.createElement('div');
     // Add the 'pixel' class to the new element
     pixel.setAttribute('class', 'pixel');
-    // Add a mouseover handler to the new element
+    // Add two mouseover handlers to the new element,
+    // one to paint and one to add text
     pixel.addEventListener('mouseover', paint);
-    // Add a rotate handler to the new element
+    pixel.addEventListener('mouseover', addText);
+
+    // Add a click handler to the new element
+    pixel.addEventListener('click', remove);
+    // Add two keydown handlers to the new element,
+    //one to rotate and one to add text to currentKey
     document.addEventListener('keydown', rotate);
+    document.addEventListener('keydown', typed);
+
 
     // Add the element to the body of the page
     document.body.appendChild(pixel);
-
   }
 }
 
@@ -59,7 +85,6 @@ function paint(e) {
   //assign those values into a single variable
   let rgbValue = `rgb(${r},${g},${b})`;
 
-
   // Change the background color of the randomized rgbValue
   pixel.style.backgroundColor = rgbValue;
 
@@ -67,6 +92,8 @@ function paint(e) {
   // When we pass additional parameters (like 'pixel' below) they
   // are passed to the callback function (resetPixel)
   setTimeout(resetPixel, PIXEL_REVERT_DELAY, pixel);
+
+
 }
 
 // resetPixel
@@ -76,36 +103,58 @@ function resetPixel(pixel) {
   pixel.style.backgroundColor = DEFAULT_COLOR;
 }
 
+// remove
+//
+//removes (sets alpha) a tile on click
+function remove(e) {
+  //find the pixel clicked and set it as the target for our HEINOUS MANIPULATIONS
+  let pixel = e.target;
+  //set opacity to 0, rendering pixel invisible (or white in this case, since the browser background is)
+  pixel.style.opacity = '0';
+
+}
 
 // keyDown
 //
 //Reacts to key presses
-function rotate(buttonPressed){
+function rotate(buttonPressed) {
 
   //find all pixels in the scene
   let pixels = document.getElementsByClassName('pixel');
 
-//for the left arrow
+  //for the left arrow
   if (buttonPressed.keyCode === 37) {
     //change the rotation variable
-    rotation += 1;
+    rotation += -2;
     // update the rotation for all pixels
-
-    for(let i=0; i < pixels.length; i++){
+    for (let i = 0; i < pixels.length; i++) {
       pixels[i].style.transform = `rotate(${rotation}deg)`;
     }
-
+  }
+  //for the right arrow
+  if (buttonPressed.keyCode === 39) {
+    //change the rotation variable
+    rotation += 2;
+    // update the rotation for all pixels
+    for (let i = 0; i < pixels.length; i++) {
+      pixels[i].style.transform = `rotate(${rotation}deg)`;
+    }
+  }
 }
 
-//for the right arrow
-if (buttonPressed.keyCode === 39) {
-  //change the rotation variable
-  rotation += -1;
-  // update the rotation for all pixels
-  for(let i=0; i < pixels.length; i++){
-    pixels[i].style.transform = `rotate(${rotation}deg)`;
-}
+//typed()
+//
+//Finds the key pressed and sets currentKey variable on that basis
+function typed(e) {
+  currentKey = e.keyCode;
+  console.log(currentKey);
 }
 
-
+//addText()
+//
+// Adds text to pixel elements (is this separate function neccessary?
+// Adding it as instructed, but couldnt I just place this in the paint function?)
+function addText(e) {
+  //add the currentKey as a string
+  e.target.innerHTML = String.fromCharCode(currentKey);
 }
