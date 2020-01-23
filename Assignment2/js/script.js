033aa85b8d96e0173ef4f974734d4919f6ed9846
Martin <martinhanses@hotmail.com>
2020-01-23 15:45:23 -0500

    A2: Followed instructions
    - Wrote code as instructed in slides (no comments)

--- Assignment2/js/script.js
+++ Assignment2/js/script.js
@@ -9,3 +9,44 @@ This is a template. Fill in the title, author, and this description
 to match your project! Write JavaScript to do amazing things below!
 
 *********************************************************************/
+let $spans;
+
+$(document).ready(setup);
+
+
+
+function setup(){
+
+  $spans = $('span');
+
+  console.log("testing");
+  setInterval(update, 500);
+  $spans.on('click', spanClicked);
+
+}
+
+
+function update(){
+
+  $spans.each(updateSpan);
+  console.log("update");
+
+}
+
+function updateSpan(){
+
+  let x = Math.random();
+  if (x < 0.1){
+    $(this).removeClass('redacted');
+    $(this).addClass('revealed');
+
+  }
+  console.log("updating spans");
+}
+
+function spanClicked(){
+
+  $(this).addClass('redacted');
+  $(this).removeClass('revealed');
+
+}
