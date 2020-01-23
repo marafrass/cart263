88d744031b2c5c236863458defea3e00026e5d3f
Martin <martinhanses@hotmail.com>
2020-01-23 16:24:14 -0500

    A2: Finished all instructions for the Redux
    - Can be found here:
    https://github.com/pippinbarr/cart263-2020/blob/master/assignments/assignment2/README.md

--- Assignment2/js/script.js
+++ Assignment2/js/script.js
@@ -10,33 +10,47 @@ to match your project! Write JavaScript to do amazing things below!
 
 *********************************************************************/
 let $spans;
+let score = 0;
+
+let secretsFound = 0;
+let secretsTotal;
+
+const UPDATEINTERVAL = 500;
+const PROBABILITYTORESET = 0.1;
 
 $(document).ready(setup);
 
+function setup() {
 
-
-function setup(){
+  secretsTotal = $('.secret').length;
+  console.log(secretsTotal);
 
   $spans = $('span');
 
   console.log("testing");
-  setInterval(update, 500);
+  setInterval(update, UPDATEINTERVAL);
   $spans.on('click', spanClicked);
+  $('.secret').on('mouseover', revealSecret)
+
+  $('#displayScore').text("Find the secrets! You've found " + secretsFound + " out of " + secretsTotal);
+
 
 }
 
 
-function update(){
+function update() {
 
   $spans.each(updateSpan);
   console.log("update");
+  console.log(secretsFound);
+
 
 }
 
-function updateSpan(){
+function updateSpan() {
 
   let x = Math.random();
-  if (x < 0.1){
+  if (x < PROBABILITYTORESET) {
     $(this).removeClass('redacted');
     $(this).addClass('revealed');
 
@@ -44,9 +58,22 @@ function updateSpan(){
   console.log("updating spans");
 }
 
-function spanClicked(){
+function spanClicked() {
 
   $(this).addClass('redacted');
   $(this).removeClass('revealed');
 
+}
+
+function revealSecret() {
+
+  $(this).addClass('found');
+  secretsFound += 1;
+  $(this).off();
+
+  if (secretsFound < secretsTotal) {
+    $('#displayScore').text("You've found " + secretsFound + " out of " + secretsTotal);
+  } else if (secretsFound === secretsTotal) {
+    $('#displayScore').text("You've done it! You've only gone and done it!");
+  }
 }
