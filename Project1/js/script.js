58e065af7894ce8bd4ce1df5655dcbec82362386
Martin <martinhanses@hotmail.com>
2020-02-04 12:13:02 -0500

    P1: Changed some variables and text.

--- Project1/js/script.js
+++ Project1/js/script.js
@@ -32,33 +32,33 @@ function logIt() {
 
 
   if (sequence === 1) {
-    $dialogue.text("TEST ONE - Alright, that worked!");
+    $dialogue.text("BOULDER - Alright. I would like you to leave me alone, now. This isn't going anywhere.");
   } else if (sequence === 2) {
-    $dialogue.text("TEST ONE - Alright, that worked!");
+    $dialogue.text("BOULDER - Alright, that worked!");
   } else if (sequence === 3) {
-    $dialogue.text("TEST ONE - Alright, that worked!");
+    $dialogue.text("BOULDER - Alright, that worked!");
   } else if (sequence === 4) {
-    $dialogue.text("TEST ONE - Alright, that worked!");
+    $dialogue.text("BOULDER - Alright, that worked!");
   } else if (sequence === 5) {
-    $dialogue.text("TEST ONE - Alright, that worked!");
+    $dialogue.text("BOULDER - Alright, that worked!");
   } else if (sequence === 6) {
-    $dialogue.text("TEST ONE - Alright, that worked!");
+    $dialogue.text("BOULDER - Alright, that worked!");
   } else if (sequence === 7) {
-    $dialogue.text("TEST ONE - Alright, that worked!");
+    $dialogue.text("BOULDER - Alright, that worked!");
   } else if (sequence === 8) {
-    $dialogue.text("TEST ONE - Alright, that worked!");
+    $dialogue.text("BOULDER - Alright, that worked!");
   } else if (sequence === 9) {
-    $dialogue.text("TEST ONE - Alright, that worked!");
+    $dialogue.text("BOULDER - Alright, that worked!");
   } else if (sequence === 10) {
-    $dialogue.text("TEST ONE - Alright, that worked!");
+    $dialogue.text("BOULDER - Alright, that worked!");
   } else if (sequence === 11) {
-    $dialogue.text("TEST ONE - Alright, that worked!");
+    $dialogue.text("BOULDER - Alright, that worked!");
   } else if (sequence === 12) {
-    $dialogue.text("TEST ONE - Alright, that worked!");
+    $dialogue.text("BOULDER - Alright, that worked!");
   } else if (sequence === 13) {
-    $dialogue.text("TEST ONE - Alright, that worked!");
+    $dialogue.text("BOULDER - Alright, that worked!");
   } else if (sequence === 14) {
-    $dialogue.text("TEST ONE - Alright, that worked!");
+    $dialogue.text("BOULDER - Alright, that worked!");
   } else {
 
 
